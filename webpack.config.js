d92e7f46942d96f3dece29cc086fb29ff0965ba4
Mats Krüger Svensson <mats@matsemann.com>
2018-04-03 20:37:42 +0200

    basic setup, webpack++

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var config = {
+    context: __dirname + '/src',
+    entry: {
+        app: './js/main.js'
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        open: false,
+        contentBase: __dirname + '/src',
+        hot: false,
+        inline: false,
+        port: 8099
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015'],
+                    plugins: ["transform-async-to-generator"]
+                }
+            }
+        ],
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: 'css-loader'
+                })
+            }
+        ]
+    },
+
+    stats: {
+        colors: true
+    },
+
+    devtool: "source-map",
+
+    plugins: [
+        new ExtractTextPlugin('styles.css'),
+    ]
+};
+
+module.exports = config;
