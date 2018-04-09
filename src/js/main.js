b61d47441c11023c26aa33d7b1e3e015b6284584
Mats Krüger Svensson <mats@matsemann.com>
2018-04-09 23:48:46 +0200

    baseball

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,15 @@
 import css from '../css/styles.css';
 import 'vis/dist/vis-network.min.css';
-import {FlowNetwork} from "./network";
 import {bipartites} from "./networks/simpleBipartite";
 import {UiState} from "./ui";
 import {longerOnes} from "./networks/longerOnes";
 import {twists} from "./networks/twists";
+import {baseball} from "./networks/baseball";
 
 
 new UiState([
     ...bipartites,
     ...longerOnes,
-    ...twists
+    ...twists,
+    ...baseball
 ]);
---
+++ src/js/networks/baseball.js
@@ -0,0 +1,91 @@
+import {FlowNetwork} from "../network";
+
+/**
+ * IMPLEMENT
+ *
+ *
+ */
+export const baseball = [
+    {
+        name: "Baseball",
+        generateNetwork: () => {
+            const net = new FlowNetwork();
+
+            const team = 4;
+            const {teams, points, remainingGames, upcomingGames} = usaData1;
+
+            // calculate our team's theoretical max score
+            const maxScore = points[team] + remainingGames[team];
+
+            // how many wins can the other teams then have?
+            const maxWins = [];
+            for (let i = 0; i < points.length; i++) {
+                maxWins[i] = maxScore - points[i];
+            }
+
+            // Make nodes for the teams, limiting their number of wins
+            for (let i = 0; i < teams.length; i++) {
+                if (i === team) continue;
+                net.createNode(teams[i]);
+                net.createEdge(teams[i], "sink", maxWins[i]);
+            }
+
+            // Try to assign a winner to each game
+            for (let i = 0; i < teams.length; i++) {
+                if (i === team) continue;
+
+                for (let j = i + 1; j < teams.length; j++) {
+                    if (j === team) continue;
+                    const gamesBetween = upcomingGames[i][j];
+                    if (gamesBetween === 0) {
+                        continue;
+                    }
+
+                    const gameName = teams[i] + " vs. " + teams[j];
+                    net.createNode(gameName);
+                    net.createEdge("source", gameName, gamesBetween);
+                    net.createEdge(gameName, teams[i], 99);
+                    net.createEdge(gameName, teams[j], 99);
+                }
+            }
+
+
+            return net;
+        }
+    }
+];
+
+
+const usaData1 = {
+    teams: ["New York", "Baltimore", "Boston", "Toronto", "Detroit"],
+    points: [74, 71, 69, 63, 49],
+    remainingGames: [28, 28, 27, 27, 27],
+    upcomingGames: [
+        [0, 3, 8, 7, 3],
+        [3, 0, 2, 7, 4],
+        [8, 2, 0, 0, 0],
+        [7, 7, 0, 0, 0],
+        [3, 4, 0, 0, 0],
+    ]
+};
+
+const worldCupData2 = {
+    teams: ["Poland", "Russia", "Brazil", "Iran", "Italy", "Cuba", "Argentina", "USA", "Japan", "Serbia", "Egypt", "China"],
+    points: [6, 5, 5, 5, 4, 4, 3, 3, 2, 1, 1, 0],
+    remainingGames: [4, 5, 5, 4, 5, 5, 5, 4, 4, 5, 4, 4],
+    upcomingGames: [
+        [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+        [1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0],
+        [1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0],
+        [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0],
+        [0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1],
+        [0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1],
+        [0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0],
+        [0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1],
+        [1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0],
+        [0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0]
+    ]
+
+};
