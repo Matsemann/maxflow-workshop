d25ea2977af83d07cf8409bd9f0e7cf88a1b3f76
Mats Krüger Svensson <mats@matsemann.com>
2018-04-09 21:09:18 +0200

    merged files

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,12 @@
 import css from '../css/styles.css';
 import 'vis/dist/vis-network.min.css';
 import {FlowNetwork} from "./network";
-import {simpleBipartite} from "./networks/simpleBipartite";
+import {bipartites} from "./networks/simpleBipartite";
 import {UiState} from "./ui";
-import {randomBipartite} from "./networks/randomBipartite";
-import {longOne} from "./networks/longerOne";
-import {evenLonger} from "./networks/evenLonger";
+import {longerOnes} from "./networks/longerOnes";
 
 
 new UiState([
-    simpleBipartite,
-    randomBipartite,
-    longOne,
-    evenLonger
+    ...bipartites,
+    ...longerOnes
 ]);
--- src/js/networks/longerOnes.js
+++ src/js/networks/longerOnes.js
@@ -1,6 +1,33 @@
 import {FlowNetwork} from "../network";
 
-export const evenLonger = {
+export const longerOnes = [{
+    name: "Long one",
+    generateNetwork: () => {
+        const net = new FlowNetwork();
+
+        net.createNode("yo");
+        net.createNode("b");
+        net.createNode("node3");
+        net.createNode("x");
+        net.createNode("five");
+        net.createNode("ggg");
+
+        net.createEdge("source", "yo", 3);
+        net.createEdge("source", "b", 4);
+        net.createEdge("source", "node3", 2);
+        net.createEdge("source", "ggg", 1);
+        net.createEdge("yo", "x", 6);
+        net.createEdge("yo", "five", 2);
+        net.createEdge("b", "node3", 9);
+        net.createEdge("node3", "five", 4);
+        net.createEdge("node3", "ggg", 9);
+        net.createEdge("x", "ggg", 6);
+        net.createEdge("five", "sink", 9);
+        net.createEdge("ggg", "sink", 9);
+
+        return net;
+    }
+}, {
     name: "Even longer one",
     generateNetwork: () => {
         const net = new FlowNetwork();
@@ -37,4 +64,4 @@ export const evenLonger = {
 
         return net;
     }
-};
+}];
